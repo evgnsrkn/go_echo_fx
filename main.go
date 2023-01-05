a9cd5c8fc57e2702d336f2092e8a15556ec8087f
evgsrkn <sorokin.lgu@gmail.com>
2023-01-05 19:47:26 +0300

    Added zap logger

--- main.go
+++ main.go
@@ -5,16 +5,19 @@ import (
 	"management/handler"
 
 	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
 	"go.uber.org/zap"
 )
 
 func main() {
 	fx.New(
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
 		fx.Provide(
 			db.NewDatabaseConnection,
 			zap.NewProduction,
 			handler.NewUserHandler,
-			NewServer,
 		),
 		fx.Invoke(
 			NewServer,
